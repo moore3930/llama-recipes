672885bb2c540369a7ed29c060a5c914464a174c
research-anonymous <moore.wods@gmail.com>
2024-11-06 22:26:38 +0100

    update

--- src/llama_recipes/utils/dataset_utils.py
+++ src/llama_recipes/utils/dataset_utils.py
@@ -9,7 +9,7 @@ from llama_recipes.utils.config_utils import get_dataloader_kwargs
 
 
 def get_preprocessed_dataset(
-    tokenizer, dataset_config, split: str = "train", *args
+    tokenizer, dataset_config, split: str = "train"
 ) -> torch.utils.data.Dataset:
     if not dataset_config.dataset in DATASET_PREPROC:
         raise NotImplementedError(f"{dataset_config.dataset} is not (yet) implemented")
@@ -25,7 +25,7 @@ def get_preprocessed_dataset(
         dataset_config,
         tokenizer,
         get_split(),
-        *args
+        dataset_config.lang_pairs
     )
 
 def get_custom_data_collator(
